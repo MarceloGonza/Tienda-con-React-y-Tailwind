45ee38f22350a01e58335512781dd7937787205a
Marcelo Gonzalez <marceloalbertogonzalez3@gmail.com>
2025-02-06 10:08:02 -0300

    aside terminado

--- Tienda react/src/components/shared/Sidebar.jsx
+++ Tienda react/src/components/shared/Sidebar.jsx
@@ -1,29 +1,81 @@
 import { RiHome6Line } from "react-icons/ri";
+import { TbCircleDashedPercentage } from "react-icons/tb";
+import { HiOutlineChartPie } from "react-icons/hi2";
+import { IoMailOutline } from "react-icons/io5";
+import { RiNotification2Line } from "react-icons/ri";
+import { IoSettingsOutline } from "react-icons/io5";
+import { IoIosLogOut } from "react-icons/io";
 
 const Sidebar = () => {
   return (
-    <div className="bg-[#1F1D2B] fixed left-0 top-0 w-28 h-full">
-      <ul className="pl-4">
-        <h1 className="text-2xl text-gray-300 uppercase font-bold text-center my-5">
-          Logo
-        </h1>
-        <li className="bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl">
-          <a
-            href="#"
-            className="bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-white"
-          >
-            <RiHome6Line className="text-2xl" />
-          </a>
-        </li>
-        <li className="hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
-          <a
-            href="#"
-            className="group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white transition-colors"
-          >
-            <RiHome6Line className="text-2xl" />
-          </a>
-        </li>
-      </ul>
+    <div className="bg-[#1F1D2B] fixed left-0 top-0 w-28 h-full flex flex-col justify-between">
+      <div>
+        <ul className="pl-4">
+          <h1 className="text-2xl text-gray-300 uppercase font-bold text-center my-5">
+            Logo
+          </h1>
+          <li className="bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl">
+            <a
+              href="#"
+              className="bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-white"
+            >
+              <RiHome6Line className="text-2xl" />
+            </a>
+          </li>
+          <li className="hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              href="#"
+              className="group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white transition-colors"
+            >
+              <TbCircleDashedPercentage className="text-2xl" />
+            </a>
+          </li>
+          <li className="hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              href="#"
+              className="group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white transition-colors"
+            >
+              <HiOutlineChartPie className="text-2xl" />
+            </a>
+          </li>
+          <li className="hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              href="#"
+              className="group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white transition-colors"
+            >
+              <IoMailOutline className="text-2xl" />
+            </a>
+          </li>
+          <li className="hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              href="#"
+              className="group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white transition-colors"
+            >
+              <RiNotification2Line className="text-2xl" />
+            </a>
+          </li>
+          <li className="hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              href="#"
+              className="group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white transition-colors"
+            >
+              <IoSettingsOutline className="text-2xl" />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul className="pl-4">
+          <li className="hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              href="#"
+              className="group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white transition-colors"
+            >
+              <IoIosLogOut className="text-2xl" />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
