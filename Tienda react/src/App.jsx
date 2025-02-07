9ba9c2a81801455356ccd7108b805ec469f7fd80
Marcelo Gonzalez <marceloalbertogonzalez3@gmail.com>
2025-02-07 11:26:38 -0300

    comienzo seccion principal

--- Tienda react/src/App.jsx
+++ Tienda react/src/App.jsx
@@ -36,6 +36,10 @@ function App() {
           {showMenu ? <IoMdClose /> : <CiMenuFries />}
         </button>
       </nav>
+      <main className="lg:pl-28 grid grid-cols-1 lg:grid-cols-8">
+        <div className="lg:col-span-6">Hola1</div>
+        <div className="lg:col-span-2 fixed lg:static right-0">Carrito</div>
+      </main>
     </div>
   );
 }
