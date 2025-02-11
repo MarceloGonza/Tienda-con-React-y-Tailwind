94401a149beabc8de23188f5ffd008b7ac808f34
Marcelo Gonzalez <marceloalbertogonzalez3@gmail.com>
2025-02-11 10:19:42 -0300

    diseño contenido responsive terminado

--- Tienda react/src/App.jsx
+++ Tienda react/src/App.jsx
@@ -38,10 +38,10 @@ function App() {
           {showMenu ? <IoMdClose /> : <CiMenuFries />}
         </button>
       </nav>
-      <main className="lg:pl-28 grid grid-cols-1 lg:grid-cols-8">
-        <div className="lg:col-span-6">
+      <main className="lg:pl-32 grid grid-cols-1 lg:grid-cols-8 p-4 pb-20">
+        <div className="lg:col-span-6 md:p-8">
           {/* header */}
-          <header className="p-4">
+          <header>
             {/* Title and search */}
             <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-4 mb-6">
               <div>
@@ -60,6 +60,7 @@ function App() {
               </form>
             </div>
             {/* Tabs */}
+
             <nav className="text-gray-300 flex items-center justify-between md:justify-start md:gap-8 border-b mb-6">
               <a
                 href="#"
@@ -78,15 +79,80 @@ function App() {
                 Grill
               </a>
             </nav>
-            {/* Title content */}
-            <div className="flex items-center justify-between">
-              <h2 className="text-xl text-gray-300">Choose Dishes</h2>
-              <button className="flex items-center gap-4 text-gray-300 bg-[#1F1D2B] py-2 px-4 rounded-lg">
-                <RiArrowDownSLine />
-                Dine In
-              </button>
-            </div>
           </header>
+
+          {/* Title content */}
+          <div className="flex items-center justify-between mb-16">
+            <h2 className="text-xl text-gray-300">Choose Dishes</h2>
+            <button className="flex items-center gap-4 text-gray-300 bg-[#1F1D2B] py-2 px-4 rounded-lg">
+              <RiArrowDownSLine />
+              Dine In
+            </button>
+          </div>
+
+          {/* Content */}
+          <div className="p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16">
+            {/* Card */}
+            <div className="bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center gap-2 text-center text-gray-300">
+              <img
+                src="plato.png"
+                className="w-40 h-40 object-cover -mt-20 shadow-2xl rounded-full"
+              />
+              <p className="text-xl">Speacy seasoned seafood nodles</p>
+              <span className="text-gray-300">$2.29</span>
+              <p className="text-gray-600">20 Bowls available</p>
+            </div>
+            {/* Card */}
+            <div className="bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center gap-2 text-center text-gray-300">
+              <img
+                src="plato.png"
+                className="w-40 h-40 object-cover -mt-20 shadow-2xl rounded-full"
+              />
+              <p className="text-xl">Speacy seasoned seafood nodles</p>
+              <span className="text-gray-300">$2.29</span>
+              <p className="text-gray-600">20 Bowls available</p>
+            </div>
+            {/* Card */}
+            <div className="bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center gap-2 text-center text-gray-300">
+              <img
+                src="plato.png"
+                className="w-40 h-40 object-cover -mt-20 shadow-2xl rounded-full"
+              />
+              <p className="text-xl">Speacy seasoned seafood nodles</p>
+              <span className="text-gray-300">$2.29</span>
+              <p className="text-gray-600">20 Bowls available</p>
+            </div>
+            {/* Card */}
+            <div className="bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center gap-2 text-center text-gray-300">
+              <img
+                src="plato.png"
+                className="w-40 h-40 object-cover -mt-20 shadow-2xl rounded-full"
+              />
+              <p className="text-xl">Speacy seasoned seafood nodles</p>
+              <span className="text-gray-300">$2.29</span>
+              <p className="text-gray-600">20 Bowls available</p>
+            </div>
+            {/* Card */}
+            <div className="bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center gap-2 text-center text-gray-300">
+              <img
+                src="plato.png"
+                className="w-40 h-40 object-cover -mt-20 shadow-2xl rounded-full"
+              />
+              <p className="text-xl">Speacy seasoned seafood nodles</p>
+              <span className="text-gray-300">$2.29</span>
+              <p className="text-gray-600">20 Bowls available</p>
+            </div>
+            {/* Card */}
+            <div className="bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center gap-2 text-center text-gray-300">
+              <img
+                src="plato.png"
+                className="w-40 h-40 object-cover -mt-20 shadow-2xl rounded-full"
+              />
+              <p className="text-xl">Speacy seasoned seafood nodles</p>
+              <span className="text-gray-300">$2.29</span>
+              <p className="text-gray-600">20 Bowls available</p>
+            </div>
+          </div>
         </div>
         <div className="lg:col-span-2 fixed lg:static right-0">Carrito</div>
       </main>
